1090185808cc436f4fdb4463719d140a36c56167
PlusRoss <yudh9662@gmail.com>
2024-06-09 23:54:47 -0700

    split file

--- run_gen_judgment.sh
+++ run_gen_judgment.sh
@@ -2,8 +2,7 @@
 
 # Command line arguments with defaults
 input_dir="${1:-None}"
-judge_model_name="$2"
-output_dir="${3:-None}"
+output_dir="${2:-None}"
 
 # Check if input directory is not 'None' and copy data from it
 if [ "$input_dir" != "None" ]; then
@@ -14,7 +13,7 @@ fi
 python gen_judgment.py --setting-file config/judge_config_test.yaml --endpoint-file config/api_config_test.yaml
 
 # Show results
-python show_result.py --judge-name "$judge_model_name"
+python show_result.py --judge-name tscience-uks-gpt4-1106
 
 # Copy results to output dir if not 'None'
 if [ "$output_dir" != "None" ]; then
