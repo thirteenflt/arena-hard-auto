cd8b4ef2b08010c10cd61cacb3f2db0ccc1fc074
PlusRoss <yudh9662@gmail.com>
2024-06-10 22:47:55 -0700

    support long model

--- run_gen_answer.sh
+++ run_gen_answer.sh
@@ -6,8 +6,13 @@ port="${2:-8019}"  # Default port value is 8019
 question_file="${3:-question.jsonl}" # Default question_file value is 'question.jsonl'
 output_dir="${4:-None}"  # Default output_dir value is 'None'
 
+model_max_length=$(jq '.max_position_embeddings' $model_name/config.json)
+# vllm max length is 8192 if model max length is greater than 8192 or model_max_length does not exist
+vllm_max_length=$((model_max_length > 8192 || model_max_length == null ? 8192 : model_max_length))
+echo "Model max length: $model_max_length, VLLM max length: $vllm_max_length"
+
 # Start the model vllm hosting
-nohup python -m vllm.entrypoints.openai.api_server --model "$model_name" --dtype auto --api-key token-abc123 --port "$port" > data/arena-hard-v0.1/server_output.log 2>&1 &
+nohup python -m vllm.entrypoints.openai.api_server --model "$model_name" --max-model-len $vllm_max_length --dtype auto --api-key token-abc123 --port "$port" > data/arena-hard-v0.1/server_output.log 2>&1 &
 
 # Wait for the server to start
 sleep 30
