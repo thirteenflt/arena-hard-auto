f98a5f34134f30f2878acdd3dd8251a19ced265d
PlusRoss <yudh9662@gmail.com>
2024-07-15 15:51:15 -0700

    support baseline model argument

--- set_config.py
+++ set_config.py
@@ -39,7 +39,11 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--judge_model_name", type=str, help="name of the judge model",
-        default="tscience-uks-gpt4-1106", choices=["tscience-uks-gpt4-1106", "tscience-uks-gpt-4o"]
+        default="tscience-uks-gpt-4o", choices=["tscience-uks-gpt4-1106", "tscience-uks-gpt-4o"]
+    )
+    parser.add_argument(
+        "--baseline_model_name", type=str, help="name of the baseline model",
+        default="tscience-uks-gpt-35-turbo-1106", choices=["tscience-uks-gpt-35-turbo-1106", "tscience-uks-gpt-35-turbo-4o"]
     )
     parser.add_argument(
         "--is_aml_run", type=str, default="True", help="if it is an AML run"
@@ -112,7 +116,7 @@ if __name__ == "__main__":
         judge_config = yaml.safe_load(file)
 
     judge_config['judge_model'] = judge_model_name
-    judge_config['baseline_model'] = "tscience-uks-gpt4-1106"
+    judge_config['baseline_model'] = args.baseline_model_name
     judge_config['model_list'] = [model_id]
     judge_config['system_prompt'] = "Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user prompt displayed below. You will be given assistant A's answer and assistant B's answer. Your job is to evaluate which assistant's answer is better.\n\nBegin your evaluation by generating your own answer to the prompt. You must provide your answers before judging any answers.\n\nWhen evaluating the assistants' answers, compare both assistants' answers with your answer. You must identify and correct any mistakes or inaccurate information.\n\nThen consider if the assistant's answers are in the same language as the user prompt. This is very important for user experience.\n\nThen consider if the assistant's answers are helpful, relevant, and concise. Helpful means the answer correctly responds to the prompt or follows the instructions. Note when user prompt has any ambiguity or more than one interpretation, it is more helpful and appropriate to ask for clarifications or more information from the user than providing an answer based on assumptions. Relevant means all parts of the response closely connect or are appropriate to what is being asked. Concise means the response is clear and not verbose or excessive.\n\nThen consider the creativity and novelty of the assistant's answers when needed. Finally, identify any missing important information in the assistants' answers that would be beneficial to include when responding to the user prompt.\n\nAfter providing your explanation, you must output only one of the following choices as your final verdict with a label:\n\n1. Assistant A is significantly better: [[A>>B]]\n2. Assistant A is slightly better: [[A>B]]\n3. Tie, relatively the same: [[A=B]]\n4. Assistant B is slightly better: [[B>A]]\n5. Assistant B is significantly better: [[B>>A]]\n\nExample output: \"My final verdict is tie: [[A=B]]\"."
     
